a4c9e4471e9b62d4cfc70c186796120b44e926d3
gahyuun <pgh123166@naver.com>
2024-04-21 11:22:11 +0900

    style: add primary color code

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1d4ed8',
+      },
+    },
   },
   plugins: [],
 };
