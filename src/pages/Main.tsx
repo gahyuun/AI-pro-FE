31f099d134fc3076ada1ec030f3d3869da397d62
xxxjinn <tlsguswls319@naver.com>
2024-09-30 16:38:24 +0900

    style: ai 채팅부분 style 수정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
-import { Button } from '../components/ui/button';
-import { Textarea } from '../components/ui/textarea';
-import { getAnswer } from '../api/gpt';
+import { useState } from "react";
+import { Button } from "../components/ui/button";
+import { Textarea } from "../components/ui/textarea";
+import { getAnswer } from "../api/gpt";
 
 export default function Main() {
-  const [textAreaValue, setTextAreaValue] = useState('');
-  const [answer, setAnswer] = useState('');
+  const [textAreaValue, setTextAreaValue] = useState("");
+  const [answer, setAnswer] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -20,35 +20,58 @@ export default function Main() {
   };
 
   return (
-    <div className="flex h-[100vh]">
+    <div className="flex h-[100vh] bg-primary">
       <div className="w-[15%] bg-gray-100 flex flex-col gap-10 p-3">
-        <h1 className="scroll-m-20 text-3xl font-bold lg:text-5xl mt-5 ">AI-PRO</h1>
+        <h1 className="scroll-m-20 text-3xl font-bold lg:text-5xl mt-5 ">
+          AI-PRO
+        </h1>
         <div className="flex flex-col">
-          <h3 className="scroll-m-20  pb-2 text-xl font-semibold tracking-tight first:mt-0">AI-PRO란?</h3>
+          <h3 className="scroll-m-20  pb-2 text-xl font-semibold tracking-tight first:mt-0">
+            AI-PRO란?
+          </h3>
           <div className="text-sm">
-            AI-PRO와 함께하는 것은 프로그래밍 여정에 도움이 되는 조수를 두는 것과 같습니다.
+            AI-PRO와 함께하는 것은 프로그래밍 여정에 도움이 되는 조수를 두는
+            것과 같습니다.
             <br />
-            <br /> 개선 사항을 관찰하고, 학습하고, 제안하여 코딩을 더욱 효율적이고 즐겁게 만듭니다.
+            <br /> 개선 사항을 관찰하고, 학습하고, 제안하여 코딩을 더욱
+            효율적이고 즐겁게 만듭니다.
             <br />
             <br />
-            AI-PRO는 여러분을 든든하게 지원하고, 더 나은 프로그래머가 되도록 도와주는 AI입니다!
+            AI-PRO는 여러분을 든든하게 지원하고, 더 나은 프로그래머가 되도록
+            도와주는 AI입니다!
           </div>
         </div>
       </div>
-      <div className="w-[85%] flex flex-col mt-10 p-6">
-        <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight ml-10">🤖 무엇을 도와드릴까요?</h2>
-        <div className="flex flex-col px-[100px] py-[30px] gap-2">
-          <Textarea value={textAreaValue} onChange={handleChange} placeholder="AI PRO에게 코드를 질문해보세요." />
-          <div className="self-end">
-            <Button variant="default" onClick={clickButton}>
+      <div className="w-[85%] flex flex-col mt-10 p-6 space-y-10">
+        <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight ml-16 text-white">
+          ✨ AI-PRO
+        </h2>
+        <div className="flex flex-col p-8 w-[85%] mx-auto my-0 bg-secondary rounded-xl">
+          <div className="flex flex-col space-y-7">
+            <div>
+              <p className="font-semibold text-white">🤖 AI 코치 </p>
+              {/*<p>{isLoading ? "답변을 준비중입니다..." : answer}</p>*/}
+              <p className="font-light text-white p-3">무엇을 도와드릴까요?</p>
+            </div>
+            <div>
+              <p className="font-semibold text-white">🙂 YOU</p>
+              <p className="font-light text-white p-3">파이썬이 뭐야?</p>
+            </div>
+          </div>
+          <div className="flex items-center rounded-xl space-x-4 mt-10 bg-black">
+            <Textarea
+              value={textAreaValue}
+              onChange={handleChange}
+              placeholder="AI PRO에게 코드를 질문해보세요."
+            />
+
+            <Button
+              variant="default"
+              className="bg-black text-white h-[100%]"
+              onClick={clickButton}
+            >
               질문하기
             </Button>
-          </div>
-        </div>
-        <div className="bg-gray-100 m-[90px] p-6 rounded-xl">
-          <p className="font-semibold">답변: </p>
-          <div className="font-light w-[100%] bg-white mt-3 rounded-md p-3">
-            <p>{isLoading ? <span className="text-slate-500">답변을 준비중입니다...</span> : answer}</p>
           </div>
         </div>
       </div>
