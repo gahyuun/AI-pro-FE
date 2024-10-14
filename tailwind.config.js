380b967b899d0d61aa4726717ec7164fd136eba3
xxxjinn <tlsguswls319@naver.com>
2024-10-14 10:37:14 +0900

    style: 색상 변수 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#1C1C26',
-        secondary: '#272836',
-        purple: 'rgb(92,0,244)',
+        primary: "#1C1C26",
+        secondary: "#272836",
+        purple: "rgb(92,0,244)",
+        customBlack: "#0D0D0D",
+        customBlack20: "#18181B",
       },
     },
   },
